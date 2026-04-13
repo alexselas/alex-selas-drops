beefac895293ddba6fe1f74e687fe3f35a7e4e61
Alex Selas <alex-selas92@hotmail.com>
2026-04-13 15:18:33 +0200

    Enable all payment methods (PayPal, card, etc.) in Stripe Checkout

--- api/create-checkout.ts
+++ api/create-checkout.ts
@@ -73,7 +73,6 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     });
 
     const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
       line_items,
       mode: 'payment',
       success_url: `${safeOrigin}?payment=success&session_id={CHECKOUT_SESSION_ID}`,
